a4e125121667b21e4289ba18ae6d37efd9cfe222
Gleb Stepanovich <stepanovich_gleb@icloud.com>
2020-12-02 20:26:26 +0300

    Initial Commit

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Gleb Stepanovich on 02.12.2020.
+//  Copyright © 2020 Gleb Stepanovich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
